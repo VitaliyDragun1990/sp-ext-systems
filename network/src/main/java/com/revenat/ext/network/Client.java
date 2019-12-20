c46a199ceb950244eac089125a98aa22b4ee0ddf
Vitaliy Dragun <vdrag00n90@gmail.com>
2019-12-20 16:50:43 +0200

    Examples of simplest multithreaded server and client

--- network/src/main/java/com/revenat/ext/network/Client.java
+++ network/src/main/java/com/revenat/ext/network/Client.java
@@ -2,6 +2,7 @@ package com.revenat.ext.network;
 
 import java.io.*;
 import java.net.Socket;
+import java.time.LocalDateTime;
 
 /**
  * @author Vitaliy Dragun
@@ -13,18 +14,33 @@ public class Client {
     }
 
     private static void sendRequest() throws IOException {
-        try (Socket socket = new Socket(Server.HOST, Server.PORT);
-             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+        for (int i = 0; i < 1000; i++) {
+            new SimpleClient().start();
+        }
+    }
 
-            String request = "Vitaliy";
+    private static class SimpleClient extends Thread {
 
-            bw.write(request);
-            bw.newLine();
-            bw.flush();
+        @Override
+        public void run() {
+            System.out.println("Started:" + LocalDateTime.now());
+            try (Socket socket = new Socket(Server.HOST, Server.PORT);
+                 BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
 
-            final String response = br.readLine();
-            System.out.println("Client got string:" + response);
+                String request = "Vitaliy";
+
+                bw.write(request);
+                bw.newLine();
+                bw.flush();
+
+                final String response = br.readLine();
+                System.out.println("Client got string:" + response);
+            } catch (IOException e) {
+                e.printStackTrace(System.out);
+            } finally {
+                System.out.println("Finished:" + LocalDateTime.now());
+            }
         }
     }
 }
--- network/src/main/java/com/revenat/ext/network/Server.java
+++ network/src/main/java/com/revenat/ext/network/Server.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import static java.lang.String.format;
+
 /**
  * @author Vitaliy Dragun
  */
@@ -12,30 +14,53 @@ public class Server {
     public static final int PORT = 25225;
 
     public static final String HOST = "127.0.0.1";
+    public static final int INCOMING_CONNECTIONS_QUEUE_MAX_SIZE = 4000;
 
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(PORT);
+        ServerSocket server = new ServerSocket(PORT, INCOMING_CONNECTIONS_QUEUE_MAX_SIZE);
 
         System.out.println("Server is started");
+        int count = 1;
         while (true) {
-            final Socket client = server.accept();
-            handleRequest(client);
+            new SimpleClientHandler(server.accept(), "handler-" + count++).start();
         }
     }
 
-    private static void handleRequest(Socket client) throws IOException {
-        try (Socket socket = client;
-             BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+    private static class SimpleClientHandler extends Thread {
+        private final Socket clientSocket;
+        private final String handlerName;
 
-            StringBuilder response = new StringBuilder("Hello, ");
-            String userName = br.readLine();
-            System.out.println("Server got string:" + userName);
+        private SimpleClientHandler(Socket socket, String handlerName) {
+            this.clientSocket = socket;
+            this.setName(handlerName);
+            this.handlerName = handlerName;
+        }
 
-            response.append(userName);
-            bw.write(response.toString());
-            bw.newLine();
-            bw.flush();
+        @Override
+        public void run() {
+            handleRequest();
+        }
+
+        private void handleRequest() {
+            try (Socket socket = clientSocket;
+                 BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+                System.out.println(format("%s started execution", handlerName));
+
+                StringBuilder response = new StringBuilder("Hello, ");
+                String userName = br.readLine();
+                System.out.println(format("%s got string:%s", handlerName, userName));
+                Thread.sleep(2000);
+
+                response.append(userName);
+                bw.write(response.toString());
+                bw.newLine();
+                bw.flush();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace(System.out);
+            } finally {
+                System.out.println(format("%s finished execution", handlerName));
+            }
         }
     }
 }

9a005bf0959121ae721a3dc6aaeb0da92459a6eb
Vitaliy Dragun <vdrag00n90@gmail.com>
2019-12-22 13:14:11 +0200

    city-register: extracting request parameters example

--- city-register/src/main/java/com/revenat/ext/cityregister/web/CheckPersonServlet.java
+++ city-register/src/main/java/com/revenat/ext/cityregister/web/CheckPersonServlet.java
@@ -1,17 +1,46 @@
 package com.revenat.ext.cityregister.web;
 
+import com.revenat.ext.cityregister.dao.PersonCheckerDao;
+import com.revenat.ext.cityregister.dao.exception.PersonCheckerException;
+import com.revenat.ext.cityregister.domain.PersonRequest;
+import com.revenat.ext.cityregister.domain.PersonResponse;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
 
 @WebServlet(name ="CheckPersonServlet", urlPatterns = "/checkPerson")
 public class CheckPersonServlet extends HttpServlet {
 
     @Override
     protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().println("Get CheckPerson - called");
+        req.setCharacterEncoding("UTF-8");
+
+        String surname = req.getParameter("surname");
+        PersonRequest request = new PersonRequest();
+        request.setSurName(surname);
+        request.setGivenName("Павел");
+        request.setPatronymic("Николаевич");
+        request.setDateOfBirth(LocalDate.of(1995, 3, 18));
+        request.setStringCode(1);
+        request.setBuilding("10");
+        request.setExtension("2");
+        request.setApartment("121");
+
+        try {
+            PersonCheckerDao dao = new PersonCheckerDao();
+            final PersonResponse response = dao.checkPerson(request);
+            if (response.isRegistered()) {
+                resp.getWriter().write("Registered");
+            } else {
+                resp.getWriter().write("Not registered");
+            }
+        } catch (PersonCheckerException e) {
+            resp.getWriter().write("Internal server error. Try again later.");
+        }
     }
 }

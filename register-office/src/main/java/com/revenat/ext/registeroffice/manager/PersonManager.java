9c1dd7773c7dd889de70d0cbf4974c52ed841c0c
Vitaliy Dragun <vdrag00n90@gmail.com>
2019-12-27 14:43:56 +0200

    register-office: simple JPA example

--- register-office/src/main/java/com/revenat/ext/registeroffice/manager/PersonManager.java
+++ register-office/src/main/java/com/revenat/ext/registeroffice/manager/PersonManager.java
@@ -3,6 +3,7 @@ package com.revenat.ext.registeroffice.manager;
 import com.revenat.ext.registeroffice.domain.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
@@ -11,6 +12,7 @@ import org.hibernate.query.Query;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.persistence.*;
 import java.util.List;
 
 /**
@@ -21,27 +23,87 @@ public class PersonManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(PersonManager.class);
 
     public static void main(final String[] args) {
+//        sessionExample();
+        jpaExample();
+    }
+
+    private static void jpaExample() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("persistence");
+
+        EntityManager entityManager = null;
+        EntityTransaction transaction = null;
+
+        // Save person
+        try {
+            entityManager = factory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+
+            Person p = new Person();
+            p.setFirstName("Anna");
+            p.setLastName("Petrova");
+            entityManager.persist(p);
+            LOGGER.info("Person was saved: {}", p);
+
+
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+
+        // Get all people
+        try {
+            entityManager = factory.createEntityManager();
+
+            final TypedQuery<Person> query = entityManager.createQuery("FROM Person", Person.class);
+            final List<Person> people = query.getResultList();
+            LOGGER.info("Get people: {}", people);
+        } finally {
+            if (entityManager != null) {
+                entityManager.close();
+            }
+        }
+    }
+
+    private static void sessionExample() {
         final SessionFactory sf = buildSessionFactory();
 
-        final Long id;
-        try (final Session session = sf.openSession()) {
-            session.getTransaction().begin();
+        Long id = null;
+        Transaction transaction = null;
+        // Save person
+        try (Session session = sf.openSession()) {
+            transaction = session.getTransaction();
+            transaction.begin();
 
             final Person p = new Person();
             p.setFirstName("Vasiliy");
             p.setLastName("Sidorov");
 
             id = (Long) session.save(p);
-            LOGGER.info("Saved person with generated id: {}", id);
+            p.setPersonId(id);
+            LOGGER.info("Person was saved: {}", p);
 
-            session.getTransaction().commit();
+            transaction.commit();
+        } catch (Exception e) {
+            LOGGER.warn(e.getMessage());
+            if (transaction != null) {
+                transaction.rollback();
+            }
         }
 
+        // Get person
         try (final Session session = sf.openSession()) {
             final Person person = session.get(Person.class, id);
             LOGGER.info("Get person: {}", person);
         }
 
+        // Get all people
         try (final Session session = sf.openSession()) {
             final Query<Person> query = session.createQuery("FROM Person", Person.class);
             final List<Person> people = query.list();
@@ -51,9 +113,12 @@ public class PersonManager {
 
     private static SessionFactory buildSessionFactory() {
         try {
-            final StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                    .configure("hibernate.cfg.xml").build();
-            final Metadata metadata = new MetadataSources(serviceRegistry).getMetadataBuilder().build();
+            final StandardServiceRegistry serviceRegistry =
+                    new StandardServiceRegistryBuilder()
+                            .configure("hibernate.cfg.xml")
+                            .build();
+            final MetadataSources metadataSources = new MetadataSources(serviceRegistry);
+            final Metadata metadata = metadataSources.getMetadataBuilder().build();
 
             return metadata.getSessionFactoryBuilder().build();
         } catch (final Exception e) {
